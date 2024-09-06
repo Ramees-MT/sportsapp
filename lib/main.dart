ce5c2a16b7493a28b1b1ed404c8eb3869e492a55
Aravind <AravindSagar2003@gmail.com>
2024-09-06 15:12:45 +0530

    teamdetails and standing details

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:sportsapp/screens/searchscreen.dart';
 import 'package:sportsapp/screens/splash.dart';
 import 'package:sportsapp/screens/splashscreen2.dart';
+import 'package:sportsapp/screens/standingdetails.dart';
 import 'package:sportsapp/screens/teamdetails.dart';
 
 void main() {
-  runApp((MaterialApp(home:Detailteam() ,)));
+  runApp((MaterialApp(home:Searchscreen() ,)));
 }
 
