5399be1f4148198837ea317a76405057f0b27dbf
SREEYUKTHA P <105417951+SREEYUKTHA-SNL@users.noreply.github.com>
2024-09-06 15:37:15 +0530

    detailteamscreen,explorescreen,articlescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:sportsapp/screens/arcticledetails.dart';
+import 'package:sportsapp/screens/explorescreen.dart';
+import 'package:sportsapp/screens/matchdetailscreen.dart';
+import 'package:sportsapp/screens/profilepage.dart';
 import 'package:sportsapp/screens/splash.dart';
 import 'package:sportsapp/screens/splashscreen2.dart';
 
 void main() {
-  runApp((MaterialApp(home:SplashScreen2() ,)));
+  runApp((MaterialApp(
+    
+    home:MatchScreen(),)));
 }
 
