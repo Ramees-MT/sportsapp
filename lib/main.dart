a5de49b2e14da8c399dbc1b920040546a52e3ed4
Aravind <AravindSagar2003@gmail.com>
2024-09-11 11:30:15 +0530

    bottomnav

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:sportsapp/screens/bottonnavi.dart';
+import 'package:sportsapp/screens/matchdetailscreen.dart';
 import 'package:sportsapp/screens/splashscreen2.dart';
 
 
 void main() {
   runApp((MaterialApp(
     
-<<<<<<< HEAD
-    home:MatchScreen(),)));
-=======
-    home:Splashscreen(),)));
->>>>>>> 107a5b159b9058742a0f03d977aca975c93069bc
+    home:BottomNavPage(),)));
 }
 
--- lib/services/apiservices.dart
+++ lib/services/apiservices.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
 import 'package:sportsapp/utils/constants.dart';
 
 class Apiservices{
